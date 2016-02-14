3964baf0067c5d46df3321cb5de83edb2175e8a0
nicolas <drabada@gmail.com>
2016-02-15 01:11:27 +0300

    add post

--- app.rb
+++ app.rb
@@ -2,7 +2,102 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_bd
+	@db = SQLite3::Database.new 'lepro.db'
+	@db.results_as_hash = true
+end
+
+
+before do
+	init_bd
+end
+
+configure do
+
+	init_bd
+
+	@db.execute 'CREATE TABLE if not exists Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT
+		)'
+
+	@db.execute 'CREATE TABLE if not exists Comments
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		post_id INTEGER
+		)'
+
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+
+	@results = @db.execute 'select * from Posts order by id desc'
+
+	erb :index
+
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+
+  content = params[:content]
+
+  if content.length <= 0
+  	@error = 'Type post text'
+  	return erb :new
+  end
+
+  # сохранение данных в бд
+
+	@db.execute 'insert into Posts (content,created_date) values (?,datetime())',[content]
+	
+	redirect to '/'
+
+end
+
+get '/details/:post_id' do
+
+	post_id=params[:post_id]
+	
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	
+	@row = results[0]
+
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
+	erb :details
+
+end
+
+post '/details/:post_id' do
+
+	post_id=params[:post_id]
+	content = params[:content]
+
+	# сохранение данных в бд
+
+	@db.execute 'insert into Comments 
+	(content,
+	created_date,
+	post_id) 
+	
+	values 
+	(?,
+	datetime(),
+	?)', [content,post_id]
+
+
+	#erb "post id #{post_id}  comment #{content}"
+
+	redirect to ('/details/' + post_id)
+
 end
